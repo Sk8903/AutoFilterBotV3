aa8f635d5385a2da82c69cb29363683059e5b234
Sk8903 <85931125+Sk8903@users.noreply.github.com>
2022-08-22 07:14:42 +0530

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,14 @@
+import pyshorteners
+import aiohttp
+import shortzy
+from import 
+
+shortz = shortzy.Shortzy(DROPLINK_API, "shareus.in")
+
+####################  droplink  ####################
+async def newlink(link):
+    print(link)
+    if LONG_DROPLINK_URL == "True" or LONG_DROPLINK_URL is True:
+        return await shortz.get_quick_link(link, silently_fail=True)
+    else:
+        return await shortz.convert(link)
